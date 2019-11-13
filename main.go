fe4719ac7328e1dd855bfcddfecf231aef9f4401
Alexis Salcedo Salgado <geiner.salcedo.ppc0111@gmail.com>
2019-11-13 01:04:23 -0500

    ajustes port

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	_ "github.com/salcedogeiner/info-credito/routers"
-
+	"strconv"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/plugins/cors"
 	"github.com/astaxie/beego/orm"
@@ -11,6 +11,10 @@ import (
 
 func main() {
 	orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("sqlconn"))
+	port, err := strconv.Atoi(beego.AppConfig.String("PORT"))
+	if err == nil {
+		beego.BConfig.Listen.HTTPPort = port
+	}
 	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
